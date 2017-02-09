d78fdeafca42d7231f6ed34ef779676478adef61
Ferdinand Agyei-Yeboah <Ferdinandyeboah@gmail.com>
2017-02-09 18:03:03 -0500

    Improved UI to include min and max limit colors. Relative intensity coloring not yet implemented.

--- map.js
+++ map.js
@@ -5,29 +5,38 @@
 //Info Window on lat/lng itself: https://developers.google.com/maps/documentation/javascript/infowindows
 //Consider using info windows on lat/lng + kml overlay
 
-//3FIX CONTROLS COLOUM SO THAT IT ALSO TAKES 90% OF VIEW HEIGHT.
+//TO DO: FIX CONTROLS COLUMN SO THAT IT ALSO TAKES 90% OF VIEW HEIGHT.
+//TO DO: SET ZOOM ON WHEN ADDING MARKERS
+//TO DO: Consider making addmarker and addmarkerarray void functions that directly mutate the model map. So that using it as a callback is smoother
+//TO DO: Consider having everything except go and clear as onchange event handlers that mutate model map. Then go uses that info and updates map.
+//TO DO: Currently if used select would have to clear and add markers every time I want to display specific data since event listeners to show info is added when markers are added
 
 var model = {
     display_squeal: false,
     something: true,
     map: null,
     markers: [],
-    squeal_image: 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png',
-    no_squeal_image: null, //null results in regular google marker
+    squeal_color: "red",
+    no_squeal_color: 'green',
     data_order: ["lat","lng","pressure","temperature","squeal"],
-    data_array: []
+    data_array: [],
+    min_color: "#fff",
+    max_color: "#000",
+    gradient_direction: "to right"
 }
 
 function init(){ //Create function
     model.map = new google.maps.Map(document.getElementById('map'), {
-      center: {lat: -34.397, lng: 150.644},
-      zoom: 8
+      center: {lat:37.646152, lng:-77.511429},
+      zoom: 17
     });
 
     //Add event listeners
     document.getElementById("go_button").addEventListener("click", updateMap)
     document.getElementById("clear_button").addEventListener("click", clearMap)
-    document.getElementById('files').addEventListener('change', showFiles, false);
+    document.getElementById("min_color").addEventListener("change", setMinColor)
+    document.getElementById("max_color").addEventListener("change", setMaxColor)
+    // document.getElementById('files').addEventListener('change', showFiles, false);
     // document.getElementById("squeal_button").addEventListener("click", updateMap) //Squeal switch. Instead maybe have the layers that they wanted.
     // document.getElementById("files").addEventListener("change", readFile)       //listener for fileuploadlocation. Changing file would
 
@@ -50,16 +59,22 @@ function readUI() {
 function updateMap() {
   //   readUI() ; //Read layer selection. Use dropdown selection
     //To DO: Call AddMarkerArray as an optional callback in readFile (Use JS pattern in JS Patterns book)
-    readFile(); //readFile's affect on model.data_array is asynchrous so have to either pass methods that depend on its affect as callbacks, therefore cannot call addArrayMarker after hoping that model.data_array has been updated
+    readFile(addMarkerArray); //readFile's affect on model.data_array is asynchrous so have to either pass methods that depend on its affect as callbacks, therefore cannot call addArrayMarker after hoping that model.data_array has been updated
     // console.log("In UpdateMap Function model.data_array is : ", model.data_array);
     // addMarkerArray(model.data_array,model.map);
 }
 
-function readFile(){
+//TO DO: PASS MARKER ARRAY AS A CALLBACK
+function readFile(callback){
 
     var UploadFileLocation = document.getElementById("files");
 
     //TO DO: Check over here if correct type of file before instaintate file reader
+
+    // check if callback is callable
+    if (typeof callback !== "function") {
+        callback = false;
+    }
 
     var reader = new FileReader();
 
@@ -70,19 +85,27 @@ function readFile(){
         var result = e.target.result;
         //result is a long string of the file contents. Split by newline and commas.
         rows = result.split("\n");
+        //split header into array. Header is first line
+        // model.data_order = rows[0].split(",");
 
+        //Start at index one to ignore header
         for(var i = 0, num_rows = rows.length; i < num_rows; i++){
             arr.push([]);
             var cells = rows[i].split(",");
             for(var j = 0, num_cols = cells.length; j < num_cols; j++){
-                arr[i].push(cells[j]);
+                arr[i].push(cells[j]);//Changed to i-1
             }
         }
         //At this point arr is a 2D array containing data passed in. Each row is a line. Each coloum in a data point.
         //NOTE: model.data_array is a jagged array containing objects representing each point.
         model.data_array = create_object_array(model.data_order,arr);
         // console.log(model.data_array);
-        addMarkerArray(model.data_array,model.map);
+        // addMarkerArray(model.data_array,model.map);
+
+        //callback
+        if (callback) {
+            callback(model.data_array,model.map);
+        }
 
     }
 
@@ -94,11 +117,11 @@ function clearMap() {
   //   init();//Simply create a new map. OR you can remove all the markers off the old map.
     model.data_array = [];
     clearMarkers(model.markers);
-    document.getElementById("input_field").innerHTML = '<input type="file" id="files" name="files[]" multiple />';
-    document.getElementById("list").innerHTML = '<p id="temp_text">Uploaded files will show up here</p>';
+    //Swap out input field to clear it. Consider storing the innerHTML into a variable and simply restoring it afterward
+    document.getElementById("input_field").innerHTML = '<input type="file" id="files" name="files[]" single />';
 
     //Re add event listener to file field since replaced input fields
-    document.getElementById('files').addEventListener('change', showFiles, false);
+    // document.getElementById('files').addEventListener('change', showFiles, false);
 }
 
 function showFiles(e) {
@@ -122,7 +145,7 @@ function ObjToString(object){
     for(var key in object) {
         if(object.hasOwnProperty(key)) {
             var value = object[key];
-            string = string + key + ": " + value + "\n";
+            string = string + "[" + key + "]" + ": " + value + "\n";
         }
     }
 
@@ -135,30 +158,55 @@ function addMarker(data, map) {
     var lat = Number(data.lat);
     var lng = Number(data.lng);
     var squeal = data.squeal.trim(); //Trim string to remove any special characters like newlines or carriage returns
-    var image;
+    var color;
     var info = ObjToString(data);
 
     if (squeal === "true"){
-        image = model.squeal_image;
+        color = model.squeal_color;
     }
     else{
-        image = model.no_squeal_image;
+        color = model.no_squeal_color;
     }
 
-    var marker = new google.maps.Marker({
-        position: {lat: lat, lng: lng},
+    //USING CIRCLES
+    var circle = new google.maps.Circle({
+        // strokeColor: '#FF0000',
+        strokeOpacity: 0.1,
+        // strokeWeight: 2,
+        fillColor: color,
+        fillOpacity: 0.95,
         map: map,
-        title: info,
-        icon: image
+        center: {lat: lat, lng: lng},
+        radius: 10
+        // radius: Math.sqrt(citymap[city].population) * 100
     });
 
-    google.maps.event.addListener(marker, 'mouseover', function(event) {
+    circle.info = info;
+
+    google.maps.event.addListener(circle, 'mouseover', function(event) {
         document.getElementById("data_location").innerHTML = info;
     });
 
-    model.markers.push(marker);
+    google.maps.event.addListener(circle, 'mouseout', function(event) {
+        document.getElementById("data_location").innerHTML = "Data Shown Here On Hover";
+    });
 
-    //TO-DO: Push marker into marker_array so that later markers can be cleared
+    //glitchy
+    // google.maps.event.addListener(circle, 'mouseover', function(event) {
+    //     document.getElementById("data_location").innerHTML = info;
+    //     // circle.strokeColor = "#F0FFFF";
+    //     circle.set("strokeColor", "#F0FFFF");
+    // });
+    //
+    //
+    // google.maps.event.addListener(circle, 'mouseout', function(event) {
+    //     document.getElementById("data_location").innerHTML = info;
+    //     // circle.strokeColor = "#F0FFFF";
+    //     circle.set("strokeColor","FF0000");
+    // });
+
+    model.markers.push(circle);
+
 }
 
 function addMarkerArray(data_array, map){
@@ -197,6 +245,32 @@ function create_object_array(data_order, data){
     }
 
     return object_array;
+}
+
+function setMinColor(e){
+    model.min_color = e.target.value;
+    var gradient = document.getElementById("color-bar");
+    gradient.style.background = 'linear-gradient('+ model.gradient_direction + ', ' + model.min_color + ', ' + model.max_color+ ')';
+
+}
+
+function setColor(e){
+    model.min_color = e.target.value || "#fff";
+    model.max_color = e.target.value || "#000";
+    var gradient = document.getElementById("color-bar");
+    gradient.style.background = 'linear-gradient('+ model.gradient_direction + ', ' + model.min_color + ', ' + model.max_color+ ')';
+
+}
+
+function setMaxColor(e){
+    model.max_color = e.target.value;
+    var gradient = document.getElementById("color-bar");
+    gradient.style.background = 'linear-gradient('+ model.gradient_direction + ', ' + model.min_color + ', ' + model.max_color+ ')';
+
+
+}
+function populateDropdown(){
+
 }
 
 /*****************************PROGRAM LOGIC***************************************/
